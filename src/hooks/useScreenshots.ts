66f2342d54274d4ec9c32d234810643cc5f99eb6
Maros <mstruk20@gmail.com>
2023-12-21 16:24:17 +0100

    Build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { Screenshot } from "../entities/Screenshot";
+import ApiClient from "../services/apiClient";
+import ms from "ms";
+
+const useScreenshots = (gameId: number) => {
+  const apiClinet = new ApiClient<Screenshot>(`/games/${gameId}/screenshots`);
+
+  return useQuery({
+    queryKey: ["screenshots", gameId],
+    queryFn: apiClinet.getAll,
+    staleTime: ms("24h"),
+  });
+};
+
+export default useScreenshots;
